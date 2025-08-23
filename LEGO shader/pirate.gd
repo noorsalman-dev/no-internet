c489fa43f08f8561a74ff36706640a0e03b76f17
noorsalman-dev <noorsalman9@proton.me>
2025-08-23 15:46:56 +0300

    weeer

--- LEGO shader/pirate.gd
+++ LEGO shader/pirate.gd
@@ -5,4 +5,4 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		animation_player
+		animation_player.play("new_animation")
